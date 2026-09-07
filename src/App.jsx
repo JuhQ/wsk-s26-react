00ab967b53cf0a5d2a297baaab3d9956b7d38603
Juha Tauriainen <juha@bin.fi>
2026-09-07 16:02:25 +0300

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
 
+import { Link, Route, Routes } from "react-router";
+
+import About from "./components/About.jsx";
 import Home from "./components/Home.jsx";
 import Welcome from "./Welcome.jsx";
 
 function App() {
   return (
     <>
-      <Home />
-      <Welcome name="Ilkka" />
-      <Welcome name="Juha" />
-      <Welcome name="Viivi" />
-      <Welcome name="Ahmed" />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
