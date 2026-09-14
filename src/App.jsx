d136ab5ea5b4e2222b1397e989059febf1f71f5b
Juha Tauriainen <juha@bin.fi>
2026-09-14 10:37:38 +0300

    Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import "./App.css";
 
-import { Link, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 
 import About from "./components/About.jsx";
 import Home from "./components/Home.jsx";
-import Welcome from "./Welcome.jsx";
+import Layout from "./components/Layout.jsx";
+import Profile from "./views/Profile.jsx";
+import Single from "./views/Single.jsx";
+import Upload from "./views/Upload.jsx";
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/upload" element={<Upload />} />
+        </Route>
       </Routes>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* basename prop is needed for browser router */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
